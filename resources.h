13eeac19b6ddfed42251c6517418cc2855062a97
Julian Nicholls <juliannicholls29@gmail.com>
2026-03-24 10:48:04 +0000

    Add score positions and make the whole window wider.

--- resources.h
+++ resources.h
@@ -16,15 +16,18 @@ inline constexpr ::Vector2 PackPosition = {3 * WindowMargin + 2 * CardWidth, Win
 inline constexpr ::Vector2 DealerRowStart = {WindowMargin, WindowMargin};
 inline constexpr ::Vector2 PlayerRowStart = {WindowMargin, 2 * WindowMargin + CardHeight};
 
+inline constexpr ::Vector2 DealerValuePosition = {WindowMargin + HandMargin * 9, DealerRowStart.y};
+inline constexpr ::Vector2 PlayerValuePosition = {WindowMargin + HandMargin * 9, PlayerRowStart.y};
+
 inline constexpr ::Vector2 ButtonSize = {110, 60};
 
-inline constexpr int Width = 2 * WindowMargin + CardWidth + HandMargin * 5 + ButtonSize.x;
+inline constexpr int Width = 2 * WindowMargin + CardWidth + HandMargin * 7 + ButtonSize.x;
 inline constexpr int Height = WindowMargin * 3 + 2 * CardHeight;
 
 inline constexpr ::Vector2 ButtonStart = {
     Width - (WindowMargin + ButtonSize.x),
     2 * WindowMargin + CardHeight};
 
-}
+} // namespace Blackjack::Constants
 
 #endif // CONSTANTS_H
